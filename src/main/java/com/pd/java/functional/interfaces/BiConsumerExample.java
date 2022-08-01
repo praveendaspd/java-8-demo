e85c261724836eebc2494ca288f48ba63e02d646
PD <praveendaspd@gmail.com>
2022-08-01 22:58:28 +1000

    BiConsumer example

--- src/main/java/com/pd/java/functional/interfaces/BiConsumerExample.java
+++ src/main/java/com/pd/java/functional/interfaces/BiConsumerExample.java
@@ -1,16 +1,31 @@
 package com.pd.java.functional.interfaces;
 
+import java.util.List;
 import java.util.function.BiConsumer;
 
 public class BiConsumerExample {
 
+	public static void printNameAndActivities(){
+
+		BiConsumer<String, List<String>> biConsumer2 = (name , activities) -> {
+			System.out.println("Name is " + name +". Activities are "+ activities);
+		};
+
+		List<Student> studentList = StudentDataBase.getAllStudents();
+		studentList.forEach(student -> biConsumer2.accept(student.getName(), student.getActivities()));
+
+	}
+
 	public static void main(String[] args) {
+
 
 		BiConsumer<String,String> biConsumer = (a,b) -> {
 			System.out.println("First name is :"+ a + " Last name is : "+b);
 		};
 
 		biConsumer.accept("Praveen" , "Das");
+
+		printNameAndActivities();
 
 		BiConsumer<Integer,Integer> multiply = (a,b) -> {
 			System.out.println("Multiplication is " + a * b);
@@ -21,5 +36,7 @@ public class BiConsumerExample {
 		};
 
 		multiply.andThen(division).accept(10,5);
+
+
 	}
 }
