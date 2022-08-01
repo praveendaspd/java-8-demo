5c2d88dc12ca3aa36b2f3d4fd03307f6a9c604c2
PD <praveendaspd@gmail.com>
2022-08-01 21:46:33 +1000

    Initial commit

--- src/main/java/com/pd/java/demo/StreamsUsages.java
+++ src/main/java/com/pd/java/demo/StreamsUsages.java
@@ -1,4 +1,7 @@
-package org.pd.java8;
+package com.pd.java.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
@@ -7,13 +10,14 @@ import java.util.stream.Collectors;
 public class StreamsUsages {
 
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(StreamsUsages.class);
 
 		List<Integer> integers = Arrays.asList(1, 2, 3, 4, 4, 4, 5, 6, 7, 7, 8, 9, 9, 10);
 
 		//Declarative style of programming - WHAT the program does?
 		List<Integer> distinctList = integers.stream()
 				.distinct().collect(Collectors.toList());
-		System.out.println(distinctList);
+		logger.info("Distinct list from Integer array : {}",distinctList);
 
 
 	}
